ad11499b0dccfffecf8b0b1e33771c20ec275068
Jack M <jackmoaby@gmail.com>
2023-10-02 12:06:41 +0100

    fix: optimisations (25ms runtime)

--- src/01_01_question.js
+++ src/01_01_question.js
@@ -1,17 +1,5 @@
 import ArrayUtils from "../utils/JS/arrayUtils.js"
 
-const array = [1, 2, 3, 4, 5, 6, 7, 8]
-const permutationArray = ArrayUtils.getPermurationArray(array)
-
-let truthyCounter = 0;
-let totalCounter = 0;
-for (let i = 0; i < permutationArray.length; i++){
-    const arr = permutationArray[i];
-    
-    totalCounter += 1
-    if (arr[0] * arr[1] * arr[2] * arr[3] * arr[4] < arr[5] * arr[6] * arr[7]){
-        truthyCounter += 1;
-    }
-}
-
-console.log(truthyCounter / totalCounter)
+const permArr = ArrayUtils.getPermutationArray([1, 2, 3, 4, 5, 6, 7, 8])
+const result = permArr.reduce((count, arr) => (arr[0] * arr[1] * arr[2] * arr[3] * arr[4] < arr[5] * arr[6] * arr[7]) ? count + 1 : count, 0) / permArr.length;
+console.log(`Result: ${result}`)
